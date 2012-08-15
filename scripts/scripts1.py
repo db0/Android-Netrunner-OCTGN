5362845b3636838450e1725e52141b825d486144
Divided by Zer0 <mail@dbzer0.com>
2012-08-15 20:25:19 +0200

    Added GPL3 Licence

--- scripts/scripts1.py
+++ scripts/scripts1.py
@@ -1,3 +1,19 @@
+    # Python Scripts for the Netrunner CCG definition for OCTGN
+    # Copyright (C) 2012  Konstantine Thoukydides
+
+    # This python script is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this script.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 Advance = ("Advance", "73b8d1f2-cd54-41a9-b689-3726b7b86f4f")
