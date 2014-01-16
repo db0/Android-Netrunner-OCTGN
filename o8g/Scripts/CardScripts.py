8218a26a9c442c8bd38dc608386552019afbe6f9
Divided by Zer0 <mail@dbzer0.com>
2014-01-16 16:24:08 +0100

    Fixed #373 (Director Haas)

--- o8g/Scripts/CardScripts.py
+++ o8g/Scripts/CardScripts.py
@@ -1991,7 +1991,7 @@ Director Haas
 -----
 bc0f047c-01b1-427f-a439-d451eda03010
 -----
-onRez:Gain1Clicks$$Gain1Max Click||onTrash:Lose1Max Click-ifActive-ifUnscored$$Lose1Clicks-ifActive-ifUnscored$$ScoreMyself-onOpponent-ifAccessed-ifUnscored-preventTrash$$Gain2Agenda Points-onOpponent-ifAccessed-ifUnscored$$Put2Scored-isSilent-ifAccessed-ifUnscored
+onRez:Gain1Clicks$$Gain1Max Click||onTrash:Lose1Max Click-ifActive-ifUnscored$$Lose1Clicks-ifActive-ifUnscored$$ScoreMyself-onOpponent-ifAccessed-ifUnscored-preventTrash-runTrashScriptWhileInactive$$Gain2Agenda Points-onOpponent-ifAccessed-ifUnscored$$Put2Scored-isSilent-ifAccessed-ifUnscored
 +++++
 
 .....
--- o8g/Scripts/meta.py
+++ o8g/Scripts/meta.py
@@ -421,8 +421,9 @@ def sendToTrash(card, pile = None): # A function which takes care of sending a c
    if card.controller != me and card.group == table: grabCardControl(card) # We take control of the card in order to avoid errors
    if card.group == table: 
       playTrashSound(card)
-      executePlayScripts(card,'TRASH') # We don't want to run automations on simply revealed cards.
       autoscriptOtherPlayers('CardTrashed',card)
+   if card.group == table or chkModulator(card, 'runTrashScriptWhileInactive', 'onTrash'): 
+      executePlayScripts(card,'TRASH') # We don't want to run automations on simply revealed cards, but some of them will like Director Haas.
    clearAttachLinks(card)
    if chkModulator(card, 'preventTrash', 'onTrash'): # IF the card has the preventTrash modulator, it's not supposed to be trashed.
       if chkModulator(card, 'ifAccessed', 'onTrash') and ds != 'runner': card.moveTo(pile) # Unless it only has that modulator active during runner access. Then when the corp trashes it, it should trash normally.
