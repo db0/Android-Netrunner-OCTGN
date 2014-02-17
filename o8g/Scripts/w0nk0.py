4e10dec2df65aa1c1b49084d15042fbbb67d31a9
W0nk0 The Sane <github@wonko.oib.com>
2014-02-17 04:24:54 +0100

    - Added Start Turn / End Turn / Grant Access buttons - Deja Vu semi-working

---
+++ o8g/Scripts/w0nk0.py
@@ -0,0 +1,12 @@
+__author__ = 'w0nk0'
+
+
+def HELP_StartEndTurnButtons(group,x=0,y=0): ##TODO w0nk0: implement respective cards -->execute="goToSot",execute="goToEndTurn"
+    """
+    ##TODO: create GUIDs 5f7ff64b-1df3-4ce8-9153-7d4370fc32b3 / cfc45a19-54ab-46c3-9e51-9b4e01b1bed0
+    pngs to /sets-markers-cards
+    create cards with ability
+    make cards trigger start/end run
+    """
+    table.create('5f7ff64b-1df3-4ce8-9153-7d4370fc32b3', -40,0, 1) #Start turn
+    table.create('cfc45a19-54ab-46c3-9e51-9b4e01b1bed0', 40, 0, 1) # end turn
