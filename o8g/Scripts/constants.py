e088aed9d8416499fc3c13343018cf66bce22e00
Divided by Zer0 <mail@dbzer0.com>
2014-05-04 12:24:35 +0200

    more subbers

--- o8g/Scripts/constants.py
+++ o8g/Scripts/constants.py
@@ -195,6 +195,7 @@ SuperchargedSubs = ['stevehouston',       # Brendan               - http://www.p
                     'Prentice78',         # Mathias Fleck         - http://www.patreon.com/user?u=120373
                     'rallenkov',          # Ralph Radtke          - http://www.patreon.com/user?u=149608
                     'tobinator',          # Tobin Lopes           - http://www.patreon.com/user?u=149628
+                    'icydevarosp',        # Nickolas Riggs        - http://www.patreon.com/user?u=151453
                     'alsciende'           # Cedric                - http://netrunnerdb.com/
                    ] # 3$ Tier
 
